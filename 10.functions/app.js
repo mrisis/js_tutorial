a8833b611529bce1c0c44eb24a0607b28cd91ee7
rezaamin <rezaamin8889@gmail.com>
2022-09-11 19:48:45 +0430

    function part

---
+++ 10.functions/app.js
@@ -0,0 +1,23 @@
+function mult(x=4,y=5){
+    return x * y
+}
+console.log(mult())
+
+///
+
+const add = function(x= 8 , y = 10){
+    return x + y
+}
+console.log(add(20,25))
+
+
+//
+const adder = (x,y) => x+y
+console.log(adder(1,4))
+
+//
+
+// (function(name){
+//     console.log('Hello '+ name);
+//   })('Ali');
+
