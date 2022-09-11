1d70905d7a278c3a165a6d3d2ec8ad5cc05c0612
rezaamin <rezaamin8889@gmail.com>
2022-09-11 14:41:49 +0430

    data type part

---
+++ data type/app.js
@@ -0,0 +1,45 @@
+//data type => primitive type , refrence type
+
+//primitive type => string , number , boolean , null ,undefined , symbol 
+
+//refrence type => objects => array , objects
+
+
+//string 
+const string = 'reza'
+console.log(typeof string)
+
+//number 
+const num = 1379
+console.log(typeof num)
+
+//boolean 
+const hasmen = true
+console.log(typeof hasmen)
+
+//
+//null 
+const car = null
+console.log(typeof car)
+
+//undefined
+let family
+console.log(typeof family)
+
+//symbol
+const sym = Symbol()
+console.log(typeof sym)
+
+//array
+
+const test = ['reeza' , 'mti']
+console.log(typeof test)
+
+
+const test2 = {
+    name : 'reza',
+    family : 'amin'
+}
+
+console.log(typeof test2)
+
