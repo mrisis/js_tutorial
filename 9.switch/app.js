e15b4f208fc8afbf577cc0a2c615da2e5f7cfca7
rezaamin <rezaamin8889@gmail.com>
2022-09-11 19:31:11 +0430

    switch part

---
+++ 9.switch/app.js
@@ -0,0 +1,15 @@
+
+
+const color = 'yellow'
+
+switch(color){
+    case 'red':
+        console.log('yes is red')
+        break
+    case 'yellow':
+        console.log('yse is yellow')
+        break
+    default : 
+    console.log('color is not red or yellow')
+    break
+}
