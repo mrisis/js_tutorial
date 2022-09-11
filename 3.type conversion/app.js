11cdfcb56b83fb814bdad3194cb4ccc1d6c97b6d
rezaamin <rezaamin8889@gmail.com>
2022-09-11 15:01:28 +0430

    type coversion part

---
+++ 3.type conversion/app.js
@@ -0,0 +1,78 @@
+
+
+let value 
+
+//number to string 
+
+// value = String(123456)
+// console.log(typeof value)
+// console.log(value.length)
+
+
+// value = String(4+4)
+// console.log(value)
+// console.log(typeof value)
+
+
+//bool to string
+
+value = String(true)
+console.log(typeof value)
+console.log(value.length)
+
+
+//date to string 
+value = String(new Date())
+console.log(typeof value)
+console.log(value)
+console.log(value.length)
+
+//array to string
+
+value = String([1,2,3])
+console.log(typeof value)
+console.log(value.length)
+
+
+
+//tostring()
+
+value = (5).toString()
+console.log(typeof value)
+console.log(value.length)
+
+
+
+//sring to number 
+
+value = Number('55')
+console.log(typeof value)
+
+
+//bool to number
+value = Number(true)
+console.log(typeof value)
+
+
+//
+// value = Number('hello')
+// console.log(typeof value)
+// console.log(value) => nan
+
+
+//
+
+value = Number([1,2,3]);
+console.log(value);
+console.log(typeof value);
+
+
+//parseint 
+value = parseInt('100.30012')
+console.log(value)
+
+//parsefloat
+value = parseFloat('100.3215')
+console.log(value)
+
+//////////////////////////////
