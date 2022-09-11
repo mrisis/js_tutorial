03548036e9cb19ab23670422c5175f0e78ada37d
rezaamin <rezaamin8889@gmail.com>
2022-09-11 14:27:48 +0430

    variables part

---
+++ variables/app.js
@@ -0,0 +1,75 @@
+//let const var 
+
+
+var name1 = "rezaamin"
+console.log(name)
+
+
+name1 = 'mahdi mirzaei'
+console.log(name)
+
+///////////////////////////
+
+//init var 
+
+// var family;
+// cosnole.log(family)
+
+// family = "znadi"
+// console.log(family)
+
+/////////////////
+
+//name of variables
+//letters , numbers , _ , $
+//CAN NOT START WITH NUMBER
+//var 1name = 'reza'  error
+
+////
+//let
+
+let hello 
+console.log(hello)
+
+
+hello='hello'
+console.log(hello)
+
+
+hello = "hello reza"
+console.log(hello)
+
+
+//cosnt
+
+const name3 = 'alireza'
+console.log(name3)
+
+// name3 = 'mamad'
+// console.log(name3)  error
+
+
+
+const people = {
+    name : 'ali',
+    family : 'amin'
+}
+
+console.log(people)
+
+
+people.name = 'alireza'
+people.family = 'mirzaei'
+
+console.log(people)
+
+
+
+//
+const numbers = [1,2,3,4]
+
+
+numbers.push(78)
+console.log(numbers)
+
+
