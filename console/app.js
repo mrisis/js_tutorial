d64cef25ccd60f30ecc3427551ede6062d68f597
rezaamin <rezaamin8889@gmail.com>
2022-09-11 14:12:14 +0430

    console part

---
+++ console/app.js
@@ -0,0 +1,33 @@
+//log to console
+
+// console.log("hello reza")
+
+
+// console.log(1234)
+
+
+// let grating = 'hello every one'
+// console.log(grating)
+
+
+
+// console.log([1,2,34])
+
+
+// console.log({a:1 ,b:2})
+
+// console.table([1,2,3,4])
+
+// console.clear()   clear page 
+
+// console.error("this is a reza error")
+
+// console.warn('this is a reza warning')
+
+// console.time('say hello')
+//     console.log('hello mahdi')
+//     console.log("hello masoud")
+//     console.log("hello mti")
+//     console.log("hello paras")
+//     console.log("hello reza")
+// console.timeEnd("say hello")
