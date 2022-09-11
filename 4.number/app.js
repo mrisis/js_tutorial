55e50fc38523c17732a8f09976f1760fe2cd654c
rezaamin <rezaamin8889@gmail.com>
2022-09-11 15:17:15 +0430

    numbers part

---
+++ 4.number/app.js
@@ -0,0 +1,91 @@
+const num1 = 20 
+const num2 = 80
+
+let value
+
+//math with numbers
+value = num1 + num2
+value = num1 - num2
+value = num1 * num2
+value = num1 / num2
+value = num1 % num2
+
+console.log(value)
+
+//Math objects
+
+//pi
+value = Math.PI
+console.log(value)
+
+
+//e
+value = Math.E
+console.log(value)
+
+
+//
+value = Math.round(5.32)
+console.log(value)
+
+
+//ceil
+
+value = Math.ceil(5.32)
+console.log(value)
+
+//floor
+value = Math.floor(5.32)
+console.log(value)
+
+
+//pow
+value = Math.pow(3,2)
+console.log(value)
+
+//abs
+value = Math.abs(-50)
+console.log(value)
+
+
+//sqrt
+value = Math.sqrt(16)
+console.log(value)
+
+
+//min
+value = Math.min(5,2,0,1,23,4,5,6,8,5,102)
+console.log(value)
+
+//max
+value = Math.max(10,0,3,0,5,1000)
+console.log(value)
+
+
+/////
+value = Math.random()
+
+value = Math.floor(Math.random() * 5 + 1)
+console.log(value)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
