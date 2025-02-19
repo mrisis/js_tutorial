248fc043a693f496a0e8b1627dfc6014461ed093
mrisis <rezaamin8889@gmail.com>
2025-02-19 18:39:11 +0330

    values and variables

--- 01-fundamentals/script.js
+++ 01-fundamentals/script.js
@@ -1,7 +1,18 @@
-alert('hello world');
+// alert('hello world');
 
-let js = 'amazing';
-if (js === 'amazing') alert('fun');
+// let js = 'amazing';
+// if (js === 'amazing') alert('fun');
 
 
-console.log(40+8+23-10);
+// console.log(40+8+23-10);
+
+
+// values and variables
+
+console.log("reza amin");
+
+let firstName = "reza";
+console.log(firstName);
+
+let PI = 3.1415;
+console.log(PI);
