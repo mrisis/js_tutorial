59e6f8f6e0cf50b758238799fc18571a26eafe03
mrisis <rezaamin8889@gmail.com>
2025-02-20 09:13:13 +0330

    data types in java script

--- 01-fundamentals/script.js
+++ 01-fundamentals/script.js
@@ -9,10 +9,32 @@
 
 // values and variables
 
-console.log("reza amin");
+// console.log("reza amin");
+
+// let firstName = "reza";
+// console.log(firstName);
+
+// let PI = 3.1415;
+// console.log(PI);
+
+
+
+// data types
+
 
 let firstName = "reza";
-console.log(firstName);
+console.log(typeof firstName)
 
-let PI = 3.1415;
-console.log(PI);
+let age = 24;
+console.log(typeof age);
+
+let jsIsFun = true;
+console.log(typeof jsIsFun);
+
+let year;
+console.log(typeof year);
+
+year = 2000;
+console.log(typeof year)
+
+console.log(typeof null);
