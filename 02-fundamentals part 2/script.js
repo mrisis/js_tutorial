be0cd01d69a31fda7e5a96ad9b5ec98a3c146c62
mrisis <rezaamin8889@gmail.com>
2025-02-22 11:50:54 +0330

    functions

--- 02-fundamentals part 2/script.js
+++ 02-fundamentals part 2/script.js
@@ -1,11 +1,36 @@
 'use strict';
 
-let hasDriversLicense = false;
-const passTest = true;
+// let hasDriversLicense = false;
+// const passTest = true;
 
-if(passTest) hasDriverLicense = true;
-if(hasDriversLicense) console.log(' i can drive!');
+// if(passTest) hasDriverLicense = true;
+// if(hasDriversLicense) console.log(' i can drive!');
 
 
 // const if = 'reza';
 // const interface = true;
+
+
+
+// functions
+
+function logger(){
+    console.log('my first name is reza');
+}
+
+logger();
+logger();
+logger();
+
+
+function fruitProcessor(apples , oranges){
+    const juice = `juice with ${apples} apples and ${oranges} oranges`
+    return juice;
+}
+
+const appleJuice = fruitProcessor(5,0);
+console.log(appleJuice);
+console.log(fruitProcessor(5,0));
+
+const appleOrangeJuice = fruitProcessor(2,4);
+console.log(appleOrangeJuice);
