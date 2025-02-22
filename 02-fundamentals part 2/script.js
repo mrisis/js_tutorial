38396cc73607beefbdcd4ac2c8be299d5ef24c3f
mrisis <rezaamin8889@gmail.com>
2025-02-22 14:10:39 +0330

    fuction declarations & expressions

--- 02-fundamentals part 2/script.js
+++ 02-fundamentals part 2/script.js
@@ -14,23 +14,43 @@
 
 // functions
 
-function logger(){
-    console.log('my first name is reza');
+// function logger(){
+//     console.log('my first name is reza');
+// }
+
+// logger();
+// logger();
+// logger();
+
+
+// function fruitProcessor(apples , oranges){
+//     const juice = `juice with ${apples} apples and ${oranges} oranges`
+//     return juice;
+// }
+
+// const appleJuice = fruitProcessor(5,0);
+// console.log(appleJuice);
+// console.log(fruitProcessor(5,0));
+
+// const appleOrangeJuice = fruitProcessor(2,4);
+// console.log(appleOrangeJuice);
+
+
+
+// function declarations
+
+function calcAge1(birthYear){
+    return 2024 - birthYear;
 }
 
-logger();
-logger();
-logger();
+const age1 = calcAge1(2000);
+console.log(age1);
 
+// fuction expressions 
 
-function fruitProcessor(apples , oranges){
-    const juice = `juice with ${apples} apples and ${oranges} oranges`
-    return juice;
+const calcAge2 = function (birthYear){
+    return 2024 - birthYear;
 }
 
-const appleJuice = fruitProcessor(5,0);
-console.log(appleJuice);
-console.log(fruitProcessor(5,0));
-
-const appleOrangeJuice = fruitProcessor(2,4);
-console.log(appleOrangeJuice);
+const age2 = calcAge2(2000);
+console.log(age2);
