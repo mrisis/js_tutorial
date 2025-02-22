26447c2cc952f287a0ef9398a36569259cd3cd6f
mrisis <rezaamin8889@gmail.com>
2025-02-22 11:24:43 +0330

    activing strict mode

---
+++ 02-fundamentals part 2/script.js
@@ -0,0 +1,11 @@
+'use strict';
+
+let hasDriversLicense = false;
+const passTest = true;
+
+if(passTest) hasDriverLicense = true;
+if(hasDriversLicense) console.log(' i can drive!');
+
+
+// const if = 'reza';
+// const interface = true;
