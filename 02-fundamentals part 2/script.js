1e057ecf0818a39618d7ea54a3d4458cb1912f65
mrisis <rezaamin8889@gmail.com>
2025-02-23 13:08:09 +0330

    introduction to objects

--- 02-fundamentals part 2/script.js
+++ 02-fundamentals part 2/script.js
@@ -205,13 +205,26 @@
 // coding challenge #2 part 2
 
 
-const calcTip = function(bill){
-    return bill >= 50 && bill <=300 ? bill * 0.15 : bill * 0.20
-};
+// const calcTip = function(bill){
+//     return bill >= 50 && bill <=300 ? bill * 0.15 : bill * 0.20
+// };
 
-const bills = [125,555,44];
-const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
-console.log(bills, tips);
+// const bills = [125,555,44];
+// const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+// console.log(bills, tips);
 
-const totals = [bills[0] + tips[0] , bills[1] + tips[1] , bills[2] + tips[2]];
-console.log(totals);
+// const totals = [bills[0] + tips[0] , bills[1] + tips[1] , bills[2] + tips[2]];
+// console.log(totals);
+
+
+// introduction to objects
+
+const reza = {
+    firstName:'reza',
+    lastName:'amin',
+    age: 2024 - 2000,
+    job: 'programmer',
+    friends: ['alireza', 'mamad'],
+}
+
+console.log(reza);
