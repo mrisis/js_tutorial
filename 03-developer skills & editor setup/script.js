86c9df85eb50872872e81e784bb5f830ce000c18
mrisis <rezaamin8889@gmail.com>
2025-02-24 13:53:38 +0330

    install live server with npm

--- 03-developer skills & editor setup/script.js
+++ 03-developer skills & editor setup/script.js
@@ -4,3 +4,9 @@ const x = 10;
 
 if (x === 23) console.log("hi");
 console.log(x);
+
+const calcAge = (birthYear) => 2024 - birthYear;
+
+console.log(calcAge(2000));
+
+if (calcAge(2000) >= 20) console.log("hi ");
