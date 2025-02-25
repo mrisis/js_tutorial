5bdc07ea4484f639255b8f6b456f17915f1c8b72
mrisis <rezaamin8889@gmail.com>
2025-02-25 15:28:09 +0330

    selecting & manipualting elements

--- 05-Guess-My-Number/script.js
+++ 05-Guess-My-Number/script.js
@@ -1,3 +1,15 @@
 "use strict";
 
 console.log(document.querySelector(".message").textContent);
+
+document.querySelector(".message").textContent = "Correct Number";
+
+document.querySelector(".number").textContent = 20;
+
+document.querySelector(".score").textContent = 35;
+
+// input fields
+
+console.log(document.querySelector(".guess").value);
+
+document.querySelector(".guess").value = 33;
