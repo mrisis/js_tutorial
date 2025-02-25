baae961d6812072530fbb82bdd0242c627639e1b
mrisis <rezaamin8889@gmail.com>
2025-02-25 10:50:57 +0330

    use debugger

--- 03-developer skills & editor setup/script.js
+++ 03-developer skills & editor setup/script.js
@@ -11,21 +11,36 @@
 
 // if (calcAge(2000) >= 20) console.log("hi ");
 
-const tempratures = [3, -2, -6, -1, "error", 9, 13, 17, 15, 14, 9, 5];
+// const tempratures = [3, -2, -6, -1, "error", 9, 13, 17, 15, 14, 9, 5];
 
-const calcTempAmplitude = function (temps) {
-  let max = temps[0];
-  let min = temps[0];
+// const calcTempAmplitude = function (temps) {
+//   let max = temps[0];
+//   let min = temps[0];
 
-  for (let i = 0; i < temps.length; i++) {
-    const curTemp = temps[i];
-    if (typeof curTemp !== "number") continue;
+//   for (let i = 0; i < temps.length; i++) {
+//     const curTemp = temps[i];
+//     if (typeof curTemp !== "number") continue;
 
-    if (curTemp > max) max = curTemp;
-    if (curTemp < min) min = curTemp;
-  }
-  return max - min;
+//     if (curTemp > max) max = curTemp;
+//     if (curTemp < min) min = curTemp;
+//   }
+//   return max - min;
+// };
+
+// const amplitude = calcTempAmplitude(tempratures);
+// console.log(amplitude);
+
+// debugger
+
+const measurKelvin = function () {
+  const measurement = {
+    type: "temp",
+    unit: "cels",
+    value: Number(prompt("Degrees celsisus:")),
+  };
+  debugger;
+  const kelvin = measurement.value + 270;
+  return kelvin;
 };
 
-const amplitude = calcTempAmplitude(tempratures);
-console.log(amplitude);
+console.log(measurKelvin());
