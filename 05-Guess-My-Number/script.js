e2ffa9206a20acc81e3be4d09ad21d6890befe8d
mrisis <rezaamin8889@gmail.com>
2025-02-26 10:05:20 +0330

    implementing the game logic

--- 05-Guess-My-Number/script.js
+++ 05-Guess-My-Number/script.js
@@ -16,11 +16,35 @@
 
 // handling click events
 
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector(".number").textContent = secretNumber;
+
+let score = 20;
+
 document.querySelector(".check").addEventListener("click", function () {
-  const guess = document.querySelector(".guess").value;
-  console.log(guess);
+  const guess = Number(document.querySelector(".guess").value);
 
   if (!guess) {
     document.querySelector(".message").textContent = "NO Number!";
+  } else if (guess === secretNumber) {
+    document.querySelector(".message").textContent = "Correct Number!✌️";
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      score--;
+      document.querySelector(".score").textContent = score;
+      document.querySelector(".message").textContent = "Too High!😁";
+    } else {
+      document.querySelector(".score").textContent = 0;
+      document.querySelector(".message").textContent = "yuo lost the game🎃";
+    }
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      score--;
+      document.querySelector(".score").textContent = score;
+      document.querySelector(".message").textContent = "Too Low!🥲";
+    } else {
+      document.querySelector(".score").textContent = 0;
+      document.querySelector(".message").textContent = "yuo lost the game🎃";
+    }
   }
 });
