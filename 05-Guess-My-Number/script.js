71ca3789fbc6e8882b360c8e5a31d7f629aa105f
mrisis <rezaamin8889@gmail.com>
2025-02-26 08:59:16 +0330

    handling click events

--- 05-Guess-My-Number/script.js
+++ 05-Guess-My-Number/script.js
@@ -1,15 +1,26 @@
 "use strict";
 
-console.log(document.querySelector(".message").textContent);
+// console.log(document.querySelector(".message").textContent);
 
-document.querySelector(".message").textContent = "Correct Number";
+// document.querySelector(".message").textContent = "Correct Number";
 
-document.querySelector(".number").textContent = 20;
+// document.querySelector(".number").textContent = 20;
 
-document.querySelector(".score").textContent = 35;
+// document.querySelector(".score").textContent = 35;
 
-// input fields
+// // input fields
 
-console.log(document.querySelector(".guess").value);
+// console.log(document.querySelector(".guess").value);
 
-document.querySelector(".guess").value = 33;
+// document.querySelector(".guess").value = 33;
+
+// handling click events
+
+document.querySelector(".check").addEventListener("click", function () {
+  const guess = document.querySelector(".guess").value;
+  console.log(guess);
+
+  if (!guess) {
+    document.querySelector(".message").textContent = "NO Number!";
+  }
+});
