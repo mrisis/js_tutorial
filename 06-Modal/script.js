64d640c6a11a938f94c8a1e9085105552f26dfc4
mrisis <rezaamin8889@gmail.com>
2025-02-26 13:57:21 +0330

    modal window project , write selection elements in variables

--- 06-Modal/script.js
+++ 06-Modal/script.js
@@ -1,1 +1,13 @@
-'use strict';
+"use strict";
+
+const modal = document.querySelector(".modal");
+
+const overly = document.querySelector(".overly");
+
+const btnCloseModal = document.querySelector(".close-modal");
+
+const btnsOpenModal = document.querySelectorAll(".show-modal");
+
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  console.log(btnsOpenModal[i].textContent);
+}
