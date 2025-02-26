4b1a9013260d9a27dee5c4a6174df8fd397cc6dc
mrisis <rezaamin8889@gmail.com>
2025-02-26 10:20:58 +0330

    manipulating css style

--- 05-Guess-My-Number/script.js
+++ 05-Guess-My-Number/script.js
@@ -24,10 +24,15 @@ let score = 20;
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
 
+  // when palyer win
+
   if (!guess) {
     document.querySelector(".message").textContent = "NO Number!";
   } else if (guess === secretNumber) {
     document.querySelector(".message").textContent = "Correct Number!✌️";
+    document.querySelector("body").style.backgroundColor = "#60b347";
+    document.querySelector(".number").style.width = "30rem";
+    // when guess too high
   } else if (guess > secretNumber) {
     if (score > 1) {
       score--;
@@ -37,6 +42,8 @@ document.querySelector(".check").addEventListener("click", function () {
       document.querySelector(".score").textContent = 0;
       document.querySelector(".message").textContent = "yuo lost the game🎃";
     }
+
+    // when guess too low
   } else if (guess < secretNumber) {
     if (score > 1) {
       score--;
