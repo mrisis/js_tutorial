2f10f38ef0275d2e64fad2634207996e454554fe
mrisis <rezaamin8889@gmail.com>
2025-03-01 15:18:26 +0330

    this keyword in practice

--- 08-Behind-the-Scenes/script.js
+++ 08-Behind-the-Scenes/script.js
@@ -29,20 +29,58 @@
 // console.log(job);
 // console.log(year);
 
-var me = "reza";
-let job = "programmer";
-const year = 2000;
+// var me = "reza";
+// let job = "programmer";
+// const year = 2000;
 
 // console.log(addDecl(2, 3));
 // console.log(addExpr(2, 3));
 // console.log(addArrow(2, 3));
 
-function addDecl(a, b) {
-  return a + b;
+// function addDecl(a, b) {
+//   return a + b;
+// }
+
+// const addExpr = function (a, b) {
+//   return a + b;
+// };
+
+// const addArrow = (a, b) => a + b;
+
+//////////
+
+// console.log(this);
+
+function calcAge(birthYear) {
+  console.log(2024 - birthYear);
+  console.log(this);
 }
 
-const addExpr = function (a, b) {
-  return a + b;
+// calcAge(2000);
+
+const calcAgeArrow = (birthYear) => {
+  console.log(2024 - birthYear);
+  console.log(this);
 };
 
-const addArrow = (a, b) => a + b;
+// calcAgeArrow(2000);
+
+const reza = {
+  year: 2000,
+  calcAge: function () {
+    console.log(2024 - this.year);
+    console.log(this);
+  },
+};
+
+reza.calcAge();
+
+const matilda = {
+  year: 2005,
+};
+
+matilda.calcAge = reza.calcAge;
+matilda.calcAge();
+
+const f = reza.calcAge;
+f();
