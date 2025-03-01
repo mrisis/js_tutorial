18b16ea112b227e5ed0c67782f338fec7ba7d382
mrisis <rezaamin8889@gmail.com>
2025-03-01 14:44:13 +0330

    hoisting & TDZ

--- 08-Behind-the-Scenes/script.js
+++ 08-Behind-the-Scenes/script.js
@@ -1,24 +1,48 @@
 "use strict";
 
-function calcAge(birthYear) {
-  const age = 2024 - birthYear;
-  console.log(firstName);
+// function calcAge(birthYear) {
+//   const age = 2024 - birthYear;
+//   console.log(firstName);
 
-  function printAge() {
-    const output = `you are age ${age} and born in ${birthYear}`;
-    console.log(output);
-    console.log(firstName);
-  }
-  if (birthYear >= 1981 && birthYear <= 1996) {
-    const str = firstName;
-    console.log(firstName);
-  }
-  printAge();
-  //   console.log(str); => refrence Error;
-  return age;
-}
+//   function printAge() {
+//     const output = `you are age ${age} and born in ${birthYear}`;
+//     console.log(output);
+//     console.log(firstName);
+//   }
+//   if (birthYear >= 1981 && birthYear <= 1996) {
+//     const str = firstName;
+//     console.log(firstName);
+//   }
+//   printAge();
+//   console.log(str); => refrence Error;
+//   return age;
+// }
 
-const firstName = "reza";
-calcAge(2000);
+// const firstName = "reza";
+// calcAge(2000);
 // printAge(); => error
 // console.log(age); => ReferenceError
+
+////////////
+
+// console.log(me);
+// console.log(job);
+// console.log(year);
+
+var me = "reza";
+let job = "programmer";
+const year = 2000;
+
+// console.log(addDecl(2, 3));
+// console.log(addExpr(2, 3));
+// console.log(addArrow(2, 3));
+
+function addDecl(a, b) {
+  return a + b;
+}
+
+const addExpr = function (a, b) {
+  return a + b;
+};
+
+const addArrow = (a, b) => a + b;
