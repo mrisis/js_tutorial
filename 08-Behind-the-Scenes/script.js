b41d618db024c2a17b2c07a46dd12a45ea8ec6cb
mrisis <rezaamin8889@gmail.com>
2025-03-01 10:40:55 +0330

    scoping in practice

--- 08-Behind-the-Scenes/script.js
+++ 08-Behind-the-Scenes/script.js
@@ -1,1 +1,24 @@
-'use strict';
+"use strict";
+
+function calcAge(birthYear) {
+  const age = 2024 - birthYear;
+  console.log(firstName);
+
+  function printAge() {
+    const output = `you are age ${age} and born in ${birthYear}`;
+    console.log(output);
+    console.log(firstName);
+  }
+  if (birthYear >= 1981 && birthYear <= 1996) {
+    const str = firstName;
+    console.log(firstName);
+  }
+  printAge();
+  //   console.log(str); => refrence Error;
+  return age;
+}
+
+const firstName = "reza";
+calcAge(2000);
+// printAge(); => error
+// console.log(age); => ReferenceError
