d1a6e345be717eaba80f2f9e17ee6fdda86d8276
mrisis <rezaamin8889@gmail.com>
2025-03-03 09:40:19 +0330

    destructring arrayes

--- 09 data structures modern operators & strings/script.js
+++ 09 data structures modern operators & strings/script.js
@@ -1,16 +1,20 @@
-'use strict';
+"use strict";
 
 // Data needed for a later exercise
 const flights =
-  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+  "_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30";
 
 // Data needed for first part of the section
 const restaurant = {
-  name: 'Classico Italiano',
-  location: 'Via Angelo Tavanti 23, Firenze, Italy',
-  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
-  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
-  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+  name: "Classico Italiano",
+  location: "Via Angelo Tavanti 23, Firenze, Italy",
+  categories: ["Italian", "Pizzeria", "Vegetarian", "Organic"],
+  starterMenu: ["Focaccia", "Bruschetta", "Garlic Bread", "Caprese Salad"],
+  mainMenu: ["Pizza", "Pasta", "Risotto"],
+
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
 
   openingHours: {
     thu: {
@@ -27,3 +31,36 @@ const restaurant = {
     },
   },
 };
+
+// destructring arrays
+
+const arr = [4, 5, 6];
+
+const [x, y, z] = arr;
+// console.log(arr);
+// console.log(x, y, z);
+// console.log(x);
+
+// const [first, second] = restaurant.categories;
+// console.log(first, second);
+
+let [first, , third] = restaurant.categories;
+console.log(first, third);
+
+[first, third] = [third, first];
+console.log(first, third);
+
+const [starter, main] = restaurant.order(2, 0);
+console.log(starter, main);
+
+// nested destructring
+
+const nested = [2, 4, [5, 6]];
+
+const [i, , [k, j]] = nested;
+console.log(i, j, k);
+
+// default values
+
+const [p = NaN, q = NaN, r = NaN] = [8, 9];
+console.log(p, q, r);
