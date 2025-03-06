33badcd260362cd3fdffa1b7acb817cb4a624993
mrisis <rezaamin8889@gmail.com>
2025-03-06 09:15:38 +0330

    default parameters

--- 10-Functions/script.js
+++ 10-Functions/script.js
@@ -1,1 +1,19 @@
-'use strict';
+"use strict";
+
+// default parameters
+
+const bookings = [];
+
+const createBooking = function (flightNum, numPassengers = 1, price = 199) {
+  const booking = {
+    flightNum,
+    numPassengers,
+    price,
+  };
+  bookings.push(booking);
+  console.log(bookings);
+};
+
+createBooking("LH123");
+createBooking("LH124", 20);
+createBooking("LH125", 205, 2550);
