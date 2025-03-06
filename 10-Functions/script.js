1bcc418e4a8f033c124612c9b69cf161a4c97b62
mrisis <rezaamin8889@gmail.com>
2025-03-06 09:36:08 +0330

    how to passing arguments works values vs refrence

--- 10-Functions/script.js
+++ 10-Functions/script.js
@@ -2,18 +2,49 @@
 
 // default parameters
 
-const bookings = [];
+// const bookings = [];
 
-const createBooking = function (flightNum, numPassengers = 1, price = 199) {
-  const booking = {
-    flightNum,
-    numPassengers,
-    price,
-  };
-  bookings.push(booking);
-  console.log(bookings);
+// const createBooking = function (flightNum, numPassengers = 1, price = 199) {
+//   const booking = {
+//     flightNum,
+//     numPassengers,
+//     price,
+//   };
+//   bookings.push(booking);
+//   console.log(bookings);
+// };
+
+// createBooking("LH123");
+// createBooking("LH124", 20);
+// createBooking("LH125", 205, 2550);
+
+// how to passing arguments works vlaues vs refrence
+
+const flight = "LH125";
+const reza = {
+  name: "reza amin",
+  passport: 242526,
 };
 
-createBooking("LH123");
-createBooking("LH124", 20);
-createBooking("LH125", 205, 2550);
+const checkIn = function (flightNum, passenger) {
+  flightNum = "LH999";
+  passenger.name = "MR." + passenger.name;
+
+  if (passenger.passport === 242526) {
+    alert("checked In");
+  } else {
+    alert("wrong PassPort");
+  }
+};
+
+checkIn(flight, reza);
+console.log(flight);
+console.log(reza);
+
+const newPassport = function (person) {
+  person.passport = Math.trunc(Math.random() * 10000000);
+};
+
+newPassport(reza);
+checkIn(flight, reza);
+console.log(reza);
