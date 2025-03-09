d1e911760740d045e55983fc32aae840f633e4b5
mrisis <rezaamin8889@gmail.com>
2025-03-09 15:23:41 +0330

    creating don elements

--- 11-Arrays-Bankist/script.js
+++ 11-Arrays-Bankist/script.js
@@ -142,24 +142,49 @@ const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 // foreach with maps & sets
 
-// maps
-currencies.forEach(function (value, key, map1) {
-  console.log(`${key}: ${value}`);
-});
+// // maps
+// currencies.forEach(function (value, key, map1) {
+//   console.log(`${key}: ${value}`);
+// });
 
-// sets
+// // sets
 
-const currenciesUnique = new Set([
-  "usd",
-  "gbp",
-  "usd",
-  "eur",
-  "eur",
-  "usd",
-  "gbp",
-]);
-console.log(currenciesUnique);
+// const currenciesUnique = new Set([
+//   "usd",
+//   "gbp",
+//   "usd",
+//   "eur",
+//   "eur",
+//   "usd",
+//   "gbp",
+// ]);
+// console.log(currenciesUnique);
 
-currenciesUnique.forEach(function (value, key, set) {
-  console.log(`${key} : ${value}`);
-});
+// currenciesUnique.forEach(function (value, key, set) {
+//   console.log(`${key} : ${value}`);
+// });
+
+// bankist project
+
+// creating dom elements
+
+const displayMovements = function (movements) {
+  containerMovements.innerHTML = "";
+
+  movements.forEach(function (movement, index) {
+    const type = movement > 0 ? "deposit" : "withdrawal";
+
+    const html = `
+    <div class="movements__row">
+          <div class="movements__type movements__type--${type}">${
+      index + 1
+    } ${type}</div>
+          <div class="movements__value">${movement}€</div>
+        </div>
+    
+    `;
+    containerMovements.insertAdjacentHTML("afterbegin", html);
+  });
+};
+
+displayMovements(account1.movements);
