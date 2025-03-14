8c9a9bf66a46dd1f72c915bc9747f5ba89e6935e
mrisis <rezaamin8889@gmail.com>
2025-03-14 09:03:29 +0330

    converting and checking numbers

--- 12-Numbers-Dates-Timers-Bankist/script.js
+++ 12-Numbers-Dates-Timers-Bankist/script.js
@@ -182,7 +182,7 @@ btnLogin.addEventListener('click', function (e) {
 
 btnTransfer.addEventListener('click', function (e) {
   e.preventDefault();
-  const amount = Number(inputTransferAmount.value);
+  const amount = +inputTransferAmount.value;
   const receiverAcc = accounts.find(
     acc => acc.username === inputTransferTo.value
   );
@@ -206,7 +206,7 @@ btnTransfer.addEventListener('click', function (e) {
 btnLoan.addEventListener('click', function (e) {
   e.preventDefault();
 
-  const amount = Number(inputLoanAmount.value);
+  const amount = +inputLoanAmount.value;
 
   if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
     // Add movement
@@ -251,3 +251,20 @@ btnSort.addEventListener('click', function (e) {
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
+
+console.log(23 === 23.0);
+
+console.log(0.1 + 0.2);
+
+console.log(Number('23'));
+console.log(+'23');
+
+console.log(Number.parseInt('24px'));
+console.log(Number.parseFloat('2.5rem'));
+
+console.log(Number.isNaN('23'));
+
+console.log(Number.isFinite('23'));
+console.log(Number.isFinite(23));
+
+console.log(Number.isInteger(23));
