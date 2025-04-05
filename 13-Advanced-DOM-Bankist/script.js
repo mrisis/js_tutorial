a6cad159905626bef9a1362a2fc591ab1e7e7e3d
mrisis <rezaamin8889@gmail.com>
2025-04-05 10:16:58 +0330

    selecting , creating , deleting elements

--- 13-Advanced-DOM-Bankist/script.js
+++ 13-Advanced-DOM-Bankist/script.js
@@ -29,3 +29,49 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+// selecting elements
+
+console.log(document.documentElement);
+
+console.log(document.head);
+
+console.log(document.body);
+
+const header = document.querySelector('.header');
+console.log(header);
+
+const allSections = document.querySelectorAll('.section');
+
+console.log(allSections);
+
+console.log(document.getElementById('section--1'));
+
+const allButtons = document.getElementsByTagName('button');
+console.log(allButtons);
+
+console.log(document.getElementsByClassName('btn'));
+
+// creating and inserting elements
+
+const message = document.createElement('div');
+
+message.classList.add('cookie-message');
+message.innerHTML =
+  "reza amin welcome back <button class = 'btn btn--close--cookie'>Got it !</button>";
+
+header.prepend(message);
+header.append(message);
+
+// header.append(message.cloneNode(true));
+
+// header.before(message);
+// header.after(message);
+
+//delete elements
+
+document
+  .querySelector('.btn--close--cookie')
+  .addEventListener('click', function () {
+    message.remove();
+  });
