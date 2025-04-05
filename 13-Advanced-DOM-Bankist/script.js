286294b606641d3ef1d73f56b7659109f969500c
mrisis <rezaamin8889@gmail.com>
2025-04-05 14:50:24 +0330

    types of events and event handlers

--- 13-Advanced-DOM-Bankist/script.js
+++ 13-Advanced-DOM-Bankist/script.js
@@ -146,3 +146,17 @@ btnScrollTo.addEventListener('click', function (e) {
 
   section1.scrollIntoView({ behavior: 'smooth' });
 });
+
+const h1 = document.querySelector('h1');
+
+const alertH1 = function (e) {
+  alert('hello reza ');
+
+  h1.removeEventListener('mouseenter', alertH1);
+};
+
+h1.addEventListener('mouseenter', alertH1);
+
+h1.onmousemove = function (e) {
+  alert('hihi');
+};
